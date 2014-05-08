aa551992d84c93a739d0edbf3f8ecfdbedff81b5
alexgus <alex.guyon78@gmail.com>
2014-05-08 23:28:24 +0200

    - Implemented ImportData

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,30 @@
 #include <cstdlib>
 
+#include <iostream>
+
 #include "ImportData.h"
 
 using namespace std;
 
 int main()
 {
+// Declaration
+	data *d;
+	ImportData *imp;
+
 // Initialize
-	ImportData *imp = new ImportData();
+	cout << "Initialize Data" << endl;
+	imp = new ImportData();
+	d = imp->getData();
+
+	cout << "Initialized constants :" << endl;
+	cout << "n    : " << d->n    << endl;
+	cout << "m    : " << d->m    << endl;
+	cout << "c    : " << d->c    << endl;
+	cout << "eta  : " << d->eta  << endl;
+	cout << "beta : " << d->beta << endl;
 
 // Begin
-
 
 // Finalize
 	delete imp;
