3b5fe3a760e02901a335ee77ab03cd6a0ebe7437
alexgus <alex.guyon78@gmail.com>
2014-05-09 19:31:34 +0200

    - Added new include for declare data

--- src/ImportData.h
+++ src/ImportData.h
@@ -10,26 +10,11 @@
 
 #include <string>
 #include <fstream>
-
 #include <iostream>
 
-#define MAX_TAU 15
-#define MAX_N	30
+#include "data.h"
 
 using namespace std;
-
-// FIXME add double value for beta (using ratio claas too)
-typedef struct inputData
-{
-	int n;				// Number batch to deliver
-	int m;				// Number of clients (client 0 is the provider)
-	int c;				// Capacity of the conveyer
-	int eta;			// Constant of the deliver cost
-	int beta;			// Storage's cost function h
-	int tau[MAX_TAU];	// Travel time between 0 and a client
-	int cl[MAX_N];		// Merchandise i to deliver to cl[i]
-	int d[MAX_N];		// Date to deliver merchandise i
-} data;
 
 /**
  * Class for importing data from a file
---
+++ src/data.h
@@ -0,0 +1,27 @@
+/*
+ * data.h
+ *
+ *  Created on: 9 mai 2014
+ *      Author: alexandre
+ */
+
+#ifndef DATA_H_
+#define DATA_H_
+
+#define MAX_TAU 15
+#define MAX_N	30
+
+// FIXME add double value for beta (using ratio claas too)
+typedef struct inputData
+{
+	int n;				// Number batch to deliver
+	int m;				// Number of clients (client 0 is the provider)
+	int c;				// Capacity of the conveyer
+	int eta;			// Constant of the deliver cost
+	int beta;			// Storage's cost function h
+	int tau[MAX_TAU];	// Travel time between 0 and a client
+	int cl[MAX_N];		// Merchandise i to deliver to cl[i]
+	int d[MAX_N];		// Date to deliver merchandise i
+} data;
+
+#endif /* DATA_H_ */
