b0b2a6d19b7842b12248f1bd778b7488491f18db
alexgus <alex.guyon78@gmail.com>
2014-05-15 18:25:51 +0200

    - Added class client

---
+++ src/Client.cpp
@@ -0,0 +1,20 @@
+/*
+ * Client.cpp
+ *
+ *  Created on: 15 mai 2014
+ *      Author: alexandre
+ */
+
+#include "Client.h"
+
+Client::Client()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Client::~Client()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Client.h
@@ -0,0 +1,21 @@
+/*
+ * Client.h
+ *
+ *  Created on: 15 mai 2014
+ *      Author: alexandre
+ */
+
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+/**
+ * Define class container for batch
+ */
+class Client
+{
+public:
+	Client();
+	virtual ~Client();
+};
+
+#endif /* CLIENT_H_ */
